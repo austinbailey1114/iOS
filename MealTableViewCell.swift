361d23539bd12911f8a9d540ead9e30503f810ee
Austin Bailey <baileyau@bc.edu>
2017-08-04 19:21:55 -0400

    Added table view from Meal tab

---
+++ MealTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  MealTableViewCell.swift
+//  LiftApp
+//
+//  Created by Austin Bailey on 8/4/17.
+//  Copyright © 2017 Austin Bailey. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var mealLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
