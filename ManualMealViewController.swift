055085e1de0d792267d90357db5d54f0059d795f
Austin Bailey <baileyau@bc.edu>
2017-08-16 09:45:13 -0400

    Added new view to allow user to input meals manually

--- ManualMealViewController.swift
+++ ManualMealViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class ManualMealViewController: UIViewController {
 
@@ -15,6 +16,10 @@ class ManualMealViewController: UIViewController {
     @IBOutlet weak var fatInput: UITextField!
     @IBOutlet weak var carbsInput: UITextField!
     @IBOutlet weak var proteinInput: UITextField!
+    
+    var user: NSManagedObject?
+    var keepContext: NSManagedObjectContext?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,12 +27,70 @@ class ManualMealViewController: UIViewController {
     }
 
     @IBAction func saveMeal(_ sender: UIButton) {
+        if calsInput.text!.doubleValue == nil || fatInput.text!.doubleValue == nil || carbsInput.text!.doubleValue == nil || proteinInput.text!.doubleValue == nil || nameInput.text! == "" {
+            createAlert(title: "Invalid Input", message: "Please ensure that no field is blank, and that each field that requires a number contains a number.")
+            return
+        }
+        user = TabController.currentUser
+        keepContext = TabController.currentContext
+        //pull date
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy"
+        let result = formatter.string(from: date)
+        //add to meal history
+        let mealHistory = user!.value(forKey: "previousMeals") as! [String]
+        let newMeal = nameInput.text! + "`" + calsInput.text! + "`" + fatInput.text! + "`" + carbsInput.text! + "`" + proteinInput.text! + "`" + result
+        let newMealHistory = [newMeal] + mealHistory
+        user!.setValue(newMealHistory, forKey: "previousMeals")
+        do {
+            try keepContext!.save()
+        }
+        catch {
+            
+        }
+        
+        calsInput.text! = ""
+        nameInput.text! = ""
+        carbsInput.text! = ""
+        fatInput.text! = ""
+        proteinInput.text! = ""
+        
+        carbsInput.resignFirstResponder()
+        nameInput.resignFirstResponder()
+        calsInput.resignFirstResponder()
+        fatInput.resignFirstResponder()
+        proteinInput.resignFirstResponder()
+        
+        
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    //close keyboard when user touches outside the keyboard
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    //close keyboard when return is hit
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        nameInput.resignFirstResponder()
+        calsInput.resignFirstResponder()
+        fatInput.resignFirstResponder()
+        carbsInput.resignFirstResponder()
+        proteinInput.resignFirstResponder()
+        return true
+    }
+
+    func createAlert (title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.cancel, handler: { (action) in
+            alert.dismiss(animated: true, completion: nil)
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
