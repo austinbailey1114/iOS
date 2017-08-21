7c4a5af83bfa131da1fa2ce58914b1ebd1283400
Austin Bailey <baileyau@bc.edu>
2017-08-21 16:20:22 -0400

    Removed login page

--- TabController.swift
+++ TabController.swift
@@ -19,6 +19,52 @@ class TabController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        //Searching Core Data for user + password
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        TabController.currentContext = context
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        request.returnsObjectsAsFaults = false
+        var userExists: Bool = false
+        do {
+            let results = try context.fetch(request) as! [NSManagedObject]
+            for result in results {
+                if result.value(forKey: "username") as! String? == ""
+                    && result.value(forKey: "password") as! String? == "" {
+                    TabController.currentUser = result
+                    userExists = true
+                }
+            }
+        }
+            
+        catch {
+            //add code
+        }
+        
+        if !userExists {
+            //create a new user in CoreData
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            let NewUser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+            NewUser.setValue("", forKey: "username")
+            NewUser.setValue("", forKey: "password")
+            NewUser.setValue([String](), forKey: "previousLifts")
+            NewUser.setValue([String](), forKey: "previousMeals")
+            NewUser.setValue([String](), forKey: "previousWeights")
+            NewUser.setValue("deadlift", forKey: "lift1")
+            NewUser.setValue(["No Type Selected"], forKey: "allLifts")
+            
+            do {
+                try context.save()
+            }
+            catch {
+                //do stuff
+            }
+            TabController.currentUser = NewUser
+            TabController.currentContext = context
+        }
         self.navigationItem.hidesBackButton = true
     }
 
